49331048f62ccdf6eb1f729468943ca16335df5a
Lorenzo <lorenzomueller1209@gmail.com>
2022-03-13 13:57:11 +0100

    Minor Changes

--- assets2/js/ad-text.js
+++ assets2/js/ad-text.js
@@ -2,10 +2,10 @@
     function getRandomInt(max) {
         return Math.floor(Math.random() * max);
     }
-    ad0='<h4><a href="https://short.speedypreneur.com/c661" target="_blank"><span style="color:#303030"><strong>If you can order a Pizza, you can make money with this system! Click here to see how</strong></span></a></h4>'
-    ad1='<h4 style="color:#303030"><a href="https://short.speedypreneur.com/vip-club" target="_blank"><span style="color:#303030"><strong>Build a wildly successful Affiliate Markting Business - Even if you have no idea what you are doing - Click here to see how</strong></span></a></h4>'
-    ad2='<h4 style="color:#303030"><a href="https://short.speedypreneur.com/make-money-blogging" target="_blank"><span style="color:#303030"><strong>Zero tech-skills? Zero experience? Earn $672.42 In 1 Day - Click here to see how</strong></span></a></h4>'
-    ad3='<h4 style="color:#303030"><a href="https://short.speedypreneur.com/bookpublishing" target="_blank"><span style="color:#303030"><strong>Make money from home by publishing Books on Amazon - Even if you are not an author or sold anything online - Click here to see how!</strong></span></a></h4>'
+    ad0='<h4><span style="font-family: helvetica, arial, sans-serif;"><a href="https://short.speedypreneur.com/c661" target="_blank" rel="noopener"><span style="color: #303030;"><strong>If you can order a Pizza, you can make money with this system! Click here to see how</strong></span></a></span></h4>'
+    ad1='<h4 style="color: #303030;"><span style="font-family: helvetica, arial, sans-serif;"><a href="https://short.speedypreneur.com/vip-club" target="_blank" rel="noopener"><span style="color: #303030;"><strong>Build a wildly successful Affiliate Markting Business - Even if you have no idea what you are doing - Click here to see how</strong></span></a></span></h4>'
+    ad2='<h4 style="color: #303030;"><span style="font-family: helvetica, arial, sans-serif;"><a href="https://short.speedypreneur.com/make-money-blogging" target="_blank" rel="noopener"><span style="color: #303030;"><strong>Zero tech-skills? Zero experience? Earn $672.42 In 1 Day - Click here to see how</strong></span></a></span></h4>'
+    ad3='<h4 style="color: #303030;"><span style="font-family: helvetica, arial, sans-serif;"><a href="https://short.speedypreneur.com/bookpublishing" target="_blank" rel="noopener"><span style="color: #303030;"><strong>Make money from home by publishing Books on Amazon - Even if you are not an author or sold anything online - Click here to see how!</strong></span></a></span></h4>'
     len=4;
     rnd=getRandomInt(len);
     document.writeln(eval("ad"+rnd));
